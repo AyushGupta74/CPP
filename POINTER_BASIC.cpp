ab56afd5603b1773512add3e4ec32dcb2031ecb7
Ayush Gupta <91776561+TaskMaster74@users.noreply.github.com>
2023-09-14 04:20:08 -0700

    Add files via upload

---
+++ POINTER_BASIC.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int a=10;
+	int* ptr1=&a;
+	int** ptr2=&ptr1;
+	cout<<ptr1<<endl;
+	cout<<ptr2<<endl;
+	cout<<*ptr1<<endl;
+	cout<<**ptr2<<endl;
+	
+	
+	cout<<a;
+	
+	
+	
+	
+}
